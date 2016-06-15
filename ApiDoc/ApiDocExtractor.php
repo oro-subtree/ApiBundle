1c825e4da6e6ea3d88db19270936deefe08d6ffc
Vova Soroka <vsoroka@magecore.com>
2016-06-15 23:56:02 +0300

    BAP-9791: Support API resources for related entities. Fix LoadEntityMetadata processor. Refactor ApiDoc functionality

---
+++ ApiDoc/ApiDocExtractor.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Oro\Bundle\ApiBundle\ApiDoc;
+
+use Nelmio\ApiDocBundle\Extractor\ApiDocExtractor as BaseExtractor;
+
+use Oro\Component\Routing\Resolver\RouteOptionsResolverAwareInterface;
+
+class ApiDocExtractor extends BaseExtractor implements RouteOptionsResolverAwareInterface
+{
+    use ApiDocExtractorTrait;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getRoutes()
+    {
+        return $this->processRoutes(parent::getRoutes());
+    }
+}
